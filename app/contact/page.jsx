1e7841d71a01a052952a32323482daed9ec86faf
Amir <suljkanovic.amir@hotmail.com>
2023-07-08 21:55:34 +0200

    Added contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,43 @@
+import Link from "next/link";
+
+
+
 export default function() {
     return (
-        <div>
-            hi
-        </div>
+        <main className="main-contact">
+            <section> 
+                <div className="contact-header">
+                    <h1>We should work <br></br> together.</h1>
+                </div> 
+                <div className="contact-email">
+                    <p>Business inquiries / Contact</p>
+                    <div className='contact-hover'>
+                        <Link href="/projects">
+                            mersim@gmail.com
+                        </Link>
+                        <span></span>
+                    </div>   
+                </div>
+                <div className="contact-socials">
+                    <p>Checkout my</p>
+                    <div className='contact-hover'>
+                        <Link href="/projects">
+                            Facebook
+                        </Link>
+                        <span></span>
+                    </div>     
+                </div>
+                <div className="contact-socials">
+                    <p>Follow me on</p>
+                    <div className='contact-hover'>
+                        <Link href="/projects">
+                            Instagram
+                        </Link>
+                        <span></span>
+                    </div>     
+                </div>
+            </section>
+
+        </main>
     );
 }
