3d7d2a3fc024b951dc93bd2fcf318f6724a104f1
Amir <suljkanovic.amir@hotmail.com>
2023-08-20 23:57:53 +0200

    Meta tags og:image - fix

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,10 @@ import { Poppins } from 'next/font/google'
 import Loading from './loading'
 import { Suspense } from 'react'
 const poppins = Poppins({ weight: '400', subsets: ['latin'] })
-
 export const metadata = {
   title: 'Mersim Suljkanovic',
   description: 'Mersim - Portfolio Website',
 }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -19,7 +17,7 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="Mersim Suljkanovic - Portfolio Website" />
         <meta name="author" content="Amir Suljkanovic" />
         <meta property="og:type" content="Portoflio Website" />
-        <meta property="og:image" content="../public/image01.png" />
+        <meta property="og:image" content="https://i.ibb.co/17QhhTH/image01.png" />
 
       </head>
       <body className={poppins.className}>
